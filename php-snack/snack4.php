b0b03697d03c416888f2f66b85095f9b09b08bdd
NicolaRebecca <master9895@gmail.com>
2021-08-26 16:44:10 +0200

    primi snack

---
+++ php-snack/snack4.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$alunni = [
+    [
+        "nome" => "OlimpiaMilano",
+        "cognome" => "Cantù",
+        "voti" = [
+            "matematica" => 8,
+            "italiano" => 7,
+            "storia" => 5
+            
+        ]
+    ],
+    [
+        "nome" => "VirtusBologna",
+        "cognome" => "VirtusRoma",
+        "voti" = [
+            "matematica" => 8,
+            "italiano" => 7,
+            "storia" => 5
+        ]
+    ],
+    [
+        "nome" => "F.C.Matera",
+        "cognome" => "Inter",
+        "voti" = [
+            "matematica" => 8,
+            "italiano" => 7,
+            "storia" => 5
+        ]
+    ],
+];
+
+?>
+
+</body>
+</html>
