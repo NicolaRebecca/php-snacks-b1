6148d46f98e732927bed2b6083ae93eebeee0bcc
NicolaRebecca <master9895@gmail.com>
2021-08-26 17:27:08 +0200

    snack 5

--- php-snack/snack3.php
+++ php-snack/snack3.php
@@ -8,6 +8,13 @@
 </head>
 <body>
     
+<!-- Creare un array di array. -->
+<!-- Ogni array figlio avrà come chiave una data in questo formato: -->
+<!-- DD/MM/YYYY (ad es 31/01/2007) e come valore un array di post associati -->
+<!-- a quella data. -->
+<!-- Stampare ogni data con i relativi post. -->
+<!--  -->
+
 <?php
 
 $posts = [
--- php-snack/snack4.php
+++ php-snack/snack4.php
@@ -8,6 +8,14 @@
 </head>
 <body>
     
+<!-- Creare un array contenente qualche alunno di un’ipotetica classe. -->
+<!-- Ogni alunno avrà i seguenti dati: -->
+<!-- nome -->
+<!-- cognome -->
+<!-- un array contenente i suoi voti scolastici -->
+<!-- Stampare nome, cognome e la media dei voti di ogni alunno. -->
+
+
 <?php
 
 $alunni = [
